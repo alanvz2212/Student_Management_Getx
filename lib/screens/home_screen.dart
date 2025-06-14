fe953ee4e98ad02021bc313c7b317359af0ea060
Alan Varghese <alanvz2212@example.com>
2025-06-14 19:15:26 +0530

    Updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:student_management_getx/controllers/student_controller.dart';
+import 'package:student_management_getx/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -40,29 +42,53 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ),
-          // Expanded(
-          //   child: Obx(() {
-          //     if (studentController.isLoading.value) {
-          //       return const Center(child: CircularProgressIndicator());
-          //     }
-          //     final students = studentController.filteredStudents;
-          //     if (students.isEmpty) {
-          //       return const Center(child: Text('No students found.'));
-          //     }
-
-          //     // return ListView.builder(
-          //     //   itemCount: students.length,
-          //     //   itemBuilder: (context, index) {
-          //     //     final student = students[index];
-          //     //     return ListTile(
-          //     //       title: Text(student.name),
-          //     //       subtitle: Text(student.admissionNumber),
-          //     //     );
-          //     //   },
-          //     // );
-          //   }
-          //   ),
-          // ),
+          Expanded(
+            child: Obx(() {
+              if (studentController.isLoading.value) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              final students = studentController.filteredStudents;
+              if (students.isEmpty) {
+                return const Center(child: Text('No Students Found'));
+              }
+              return studentController.isGridView.value
+                  ? GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 0.75,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                      ),
+                      itemCount: students.length,
+                      itemBuilder: (context, index) {
+                        final student = students[index];
+                        return buildStudentCard(
+                          context,
+                          student,
+                          studentController,
+                        );
+                      },
+                    )
+                  : ListView.builder(
+                      itemCount: students.length,
+                      itemBuilder: (context, index) {
+                        final student = students[index];
+                        return ListTile(
+                          leading: CircleAvatar(
+                            radius: 30,
+                            backgroundImage:
+                                studentController.imageFile.value != null
+                                ? FileImage(studentController.imageFile.value!)
+                                : AssetImage('assets/images/default_avatar.png')
+                                      as ImageProvider,
+                          ),
+                          title: Text(student.name),
+                          subtitle: Text(student.admissionNumber),
+                        );
+                      },
+                    );
+            }),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(

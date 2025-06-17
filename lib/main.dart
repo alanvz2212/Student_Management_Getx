7251e08f0c64e86c81f0411cd9528c2bfca389aa
Alan Varghese <alanvz2212@example.com>
2025-06-17 21:28:28 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:student_management_getx/screens/splash_screen.dart';
-
-void main() {
-  runApp(const MyApp());
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Student Management',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const SplashScreen(),
-    );
+    return const Placeholder();
   }
 }
